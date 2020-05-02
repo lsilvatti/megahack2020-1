0fb35b56ce4920283781db9daa355be2da2d881a
MatheusGodoiWin10 <mt_godoi@hotmail.com>
2020-05-02 12:08:01 -0300

    Made a test tab for the Tips tab with tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import tipData from '../../assets/json/tip-cards.json';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  public tipsArray = [];
 
-  constructor() {}
+  constructor() {
+    this.feedArray();
+  }
 
+  feedArray(){
+    tipData.forEach(element => {
+      this.tipsArray.push(element)
+    });
+  }  
 }
+
