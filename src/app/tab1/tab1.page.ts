39b7d694872578701d8857b244adf59176fedf71
MatheusGodoiWin10 <mt_godoi@hotmail.com>
2020-05-03 04:08:07 -0300

    Now with dynamic objects appearing on screen

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Pipe } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import spendData from '../../assets/json/spend-data.json';
 import playerData from '../../assets/json/player-data.json';
 
+@Pipe({name: 'arrayObjects',pure:false})
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -18,9 +20,9 @@ export class Tab1Page {
   public objective = "Economizar R$ 1500"
   public playerImg = "./assets/imgs/boneco.png"
   public playerRoom = null;
-  public arrayObjects = [{}];
+  public obtainedObjects = [];
 
-  constructor() {
+  constructor(public alertController: AlertController) {
     this.getSpentValues()
     this.getDescription()
     this.getPlayerRoom()
@@ -37,11 +39,11 @@ export class Tab1Page {
   }
 
   getSpentValues() {
-    for(var i=0; i < spendData.length; i++) {
-      if (spendData[i].type == "income") {
-        this.saved += parseFloat(spendData[i].value);
+    for(var i=0; i < spendData[0].spending.length; i++) {
+      if (spendData[0].spending[i].type == "income") {
+        this.saved += parseFloat(spendData[0].spending[i].value);
       } else {
-        this.spent += parseFloat(spendData[i].value);
+        this.spent += parseFloat(spendData[0].spending[i].value);
       }
     } 
   }
@@ -60,12 +62,82 @@ export class Tab1Page {
 
   getPlayerRoom() {
     this.playerRoom = playerData[0].room;
-    var temp = {"img": null, "class": null};
     for(var i = 0; i < playerData[0].objects.length; i++){
-      temp.img = playerData[0].objects[i].objectimg;
-      temp.class = playerData[0].objects[i].objectclass;
+      var temp = playerData[0].objects[i].objectname;
 
-      this.arrayObjects.push({"img": temp.img, "class": temp.class});
+      if (temp == "mesa"){
+        this.obtainedObjects[0] = true;
+      }else if (temp == "lampada"){
+        this.obtainedObjects[1] = true;
+      }else if (temp == "quadro"){
+        this.obtainedObjects[2] = true;
+      }
     }
+  }
+
+  async registerTransation() {
+    const alert = await this.alertController.create({
+      header: 'Registrar Transação',
+      inputs: [
+        {
+          name: 'transName',
+          type: 'text',
+          placeholder: 'Nome da Transação'
+        },
+        {
+          name: 'transValue',
+          type: 'number',
+          min: 0,
+          placeholder: 'Valor da Transação'
+        }],
+      buttons: [
+        {
+          text: 'Ganho',
+          role: 'submit',
+          cssClass: 'success',
+          handler: (data) => {
+            this.submitTransaction(data.transName, data.transValue, "income");
+          }
+        },
+        {
+          text: 'Gasto',
+          role: 'submit',
+          cssClass: 'danger',
+          handler: (data) => {
+            console.log(data); 
+            this.submitTransaction(data.transName, data.transValue, "outcome");
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'medium',
+          handler: () => {
+            console.log();
+          }
+        }]
+    });
+    await alert.present();
+  }
+
+  submitTransaction(name, value, type){
+    if(value <= 0 ){
+      return;
+    }
+    spendData[0].spending.push({"title": name, "type": type, "value": value})
+
+    if (type == "income"){
+      this.saved += parseFloat(value);
+      if((this.saved >= 1500)&&(!this.obtainedObjects[2])){
+        this.obtainedObjects[2] = true;
+      }
+      if((this.saved >= 800)&&(!this.obtainedObjects[1])){
+        this.obtainedObjects[1] = true;
+      }
+    }else if (type == "outcome"){
+      this.spent += parseFloat(value);
+    }
+
+    this.getDescription()
   }
 }
